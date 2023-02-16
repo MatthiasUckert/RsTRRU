acadc9ce21ac40cec42ba6501c9ae07f650c9f28
Matthias Uckert <Matthias.Uckert@Outlook.com>
2023-02-16 15:49:31 +0100

    Fixed Typo Added second project selection Added dummy descriptions for selection

--- 1_code/app.R
+++ 1_code/app.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(dplyr)
 
 # Define the u-shaped curve function
 u_shape <- function(x) {
@@ -6,28 +7,39 @@ u_shape <- function(x) {
   return(y)
 }
 
+choice <- c(
+  "A01", "A02", "A03", "A04", "A05",
+  "A06", "A07", "A08", "B01", "B02",
+  "B03", "B04", "B05", "B06", "B07",
+  "B08", "B09", "B10", "C01", "C02", "C03"
+)
+
 # Define the Shiny UI
 ui <- fluidPage(
-  titlePanel("Rate Your Project"),
+  titlePanel("Classify Your Project"),
   sidebarLayout(
     sidebarPanel(
       HTML("<h4>Instructions:</h4>"),
-      HTML("<p>Move the blue point along the curve by clicking on the curve.</p>"),
+      HTML("<p>Move the red point along the curve by clicking on the curve.</p>"),
       actionButton("save_button", "Save Response"),
       br(),
       br(),
       shinyWidgets::radioGroupButtons(
-        inputId = "radio_group",
-        label = "Choose an option:",
-        choices = c(
-          "A01", "A02", "A03", "A04", "A05",
-          "A06", "A07", "A08", "B01", "B02",
-          "B03", "B04", "B05", "B06", "B07",
-          "B08", "B09", "B10", "C01", "C02", "C03"
-        ), individual = TRUE
-      )
+        inputId = "group_your_project",
+        label = "Please choose your project",
+        choices = choice, 
+        individual = TRUE
+      ),
+      shinyWidgets::radioGroupButtons(
+        inputId = "group_rate_project",
+        label = "Please choose the project you want to rate",
+        choices = choice, 
+        individual = TRUE
+      ),
+      verbatimTextOutput("choice")
     ),
     mainPanel(
+      HTML("<h4>Transparancy-Outcome Curve</h4>"),
       plotOutput("plot", height = "600px", hover = "plot_hover", click = "plot_click"),
       br(),
       tags$div(
@@ -77,13 +89,40 @@ server <- function(input, output, session) {
   # Update the point when the mouse is moved
   observeEvent(input$plot_click, {
     # Get the x and y coordinates of the mouse
-    point$x <- input$plot_click$x
-    point$y <- u_shape(input$plot_click$x)
+    if (input$plot_click$x < -5) {
+      point$x <- -5
+      point$y <- u_shape(-5)
+    } else if (input$plot_click$x > 5) {
+      point$x <- 5
+      point$y <- u_shape(5)
+    } else {
+      point$x <- input$plot_click$x
+      point$y <- u_shape(input$plot_click$x)
+    }
+
   })
   
   # Display the coordinates of the point
   output$point_coords <- renderText({
-    paste("Point coordinates: (", round(point$x, 2), ", ", round(point$y, 2), ")")
+    text_ <- case_when(
+      point$x <= -5   ~ "Are you sure you study transparancy? :-D",
+      point$x <= -2.5 ~ "Your project studies a setting where we have limited transparancy and in turn a limited outcome",
+      point$x > -.5 & point$x < .5    ~ "Congratulation the transparancy within your project achieves the highest outcome",
+      point$x < 0     ~ "Your project studies a setting where we have decent transparancy and a high outcome",
+      point$x <= 2.5  ~ "Your project studies a setting where we have high transparancy but only a limited outcome",
+      point$x < 5     ~ "Your project studies a setting where we have extremely high transparancy with adverse effects on the outcome",
+      point$x >= 5    ~ "Maximum transparancy but no outcome! :("
+    )
+    
+    paste("Point coordinates: (", round(point$x, 2), ", ", round(point$y, 2), ")", "\n", text_)
+  })
+  
+  output$choice <- renderText({
+case_when(
+      input$group_your_project == input$group_rate_project ~ "You want to rate your own project",
+      input$group_your_project != input$group_rate_project ~ "Are you sure you want to rate another project?"
+    )
+    
   })
 }
 
